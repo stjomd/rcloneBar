77719fec8faa3cfd83f2be51e9eefe897e9accea
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2024-09-02 20:30:46 +0200

    Initial Commit

---
+++ rcloneBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rcloneBar
+//
+//  Created by Artem Zhukov on 02.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rcloneBar/rcloneBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  rcloneBarApp.swift
+//  rcloneBar
+//
+//  Created by Artem Zhukov on 02.09.24.
+//
+
+import SwiftUI
+
+@main
+struct rcloneBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
